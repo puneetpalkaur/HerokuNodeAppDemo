d5728bc1bf479c61d52d1bc8523fb7e61533ef4e
puneetpalkaur <billingpuneet.13@gmail.com>
2020-12-19 19:25:10 -0500

    First Commit

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ creates a server listening on port that returns a 200 and text "Bonjour World"
 */
 
 let http = require('http');
-http.createServer((request, response)=>{
-    response.status(200).send('Bonjour World!');
+http.createServer((request, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+
 }).listen(process.env.PORT);
 
